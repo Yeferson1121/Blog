9046f9f64d4ac29f814fb3849ccfd47cfcf787fe
Yeferson Tapias Ramirez <tapiasyeferson@gmail.com>
2021-04-23 20:17:32 -0500

    correcion de ubicacion de la coma

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ document.querySelector ('.nav-menu').classList.toggle('show');
 }) ;
 
 ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.showcase' , { delay: 500});
-ScrollReveal().reveal('.new-cards' , { delay: 500});
-ScrollReveal().reveal('.cards-banner-one' , { delay: 500});
+ScrollReveal().reveal('.showcase', { delay: 500});
+ScrollReveal().reveal('.new-cards', { delay: 500});
+ScrollReveal().reveal('.cards-banner-one', { delay: 500});
