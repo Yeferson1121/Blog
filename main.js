371508c32a4d61ddccc0bf31af13b01a345f99cb
Yeferson Tapias Ramirez <tapiasyeferson@gmail.com>
2021-04-23 20:10:59 -0500

    se creo la lógica para darle un efecto de desvanicion a la pagina e igual que un delay

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 document.querySelector(".menu-btn").addEventListener('click', () => {
 document.querySelector ('.nav-menu').classList.toggle('show');
 }) ;
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase' , { delay: 500});
+ScrollReveal().reveal('.new-cards' , { delay: 500});
+ScrollReveal().reveal('.cards-banner-one' , { delay: 500});
